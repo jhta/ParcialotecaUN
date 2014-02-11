8378f440e3740df26a0d0ff78c858e9452a77ccc
Jonathan <jonathanvm13@gmail.com>
2014-02-11 17:11:47 -0500

    marica

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:nombre, :tipo, :descripcion, :fecha, :peso, :formato, :id_materia)
+      params.require(:item).permit(:nombre, :tipo, :descripcion, :fecha, :peso, :formato, :id_materia, :archivo)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,31 @@
 class Item < ActiveRecord::Base
+	after_save :guardar_archivo
+	ITEMS = File.join Rails.root, 'public', 'archivo_store'
+	def archivo=(file_data)
+		unless file_data.blank?
+			@file_data = file_data
+			self.formato = file_data.original_filename.split('.').last.downcase
+			self.peso = 20 # File.size(archivo_filename)
+		end
+	end
+	def archivo_filename
+		File.join ITEMS, "#{id}.#{formato}"
+	end
+	def archivo_path
+		"/archivo_store/#{id}.#{formato}"
+	end
+	def has_archivo?
+		File.exists? archivo_filename
+	end
+
+	private
+	def guardar_archivo
+		if @file_data
+			FileUtils.mkdir_p ITEMS
+			File.open(archivo_filename, 'wb') do |f|
+				f.write(@file_data.read)
+			end
+			@file_data=nil
+		end
+	end
 end
